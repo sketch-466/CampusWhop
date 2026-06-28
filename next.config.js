d7c7776314c5fe102c1729be413d21b84d307462
Sketch-466 <mathewisaac466@gmail.com>
2026-06-28 23:41:03 +0100

    git add . git commit -m "chore: add all root config files" git push

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
- 
- 
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
