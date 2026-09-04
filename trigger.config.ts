b665a85327d631b736617627cce5edd375654f15
Sketch-466 <mathewisaac466@gmail.com>
2026-09-04 21:28:05 +0100

    update trigger.config.ts

--- trigger.config.ts
+++ trigger.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@trigger.dev/sdk";
 
 export default defineConfig({
   project: "proj_ekuafgrxxqgsywaosgun",
-  runtime: "node@24",
+  runtime: "node-24",
   logLevel: "log",
   maxDuration: 300,
   dirs: ["./src/trigger"],
